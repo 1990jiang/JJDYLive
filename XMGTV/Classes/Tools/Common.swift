dd791dfc05c8f1c11028daac60f22a81eef0591b
Tmac <562752212@qq.com>
2017-03-30 15:13:13 +0800

    集成这个RollPageTitleView，可以重点研究下这个封装思想

---
+++ XMGTV/Classes/Tools/Common.swift
@@ -0,0 +1,17 @@
+//
+//  Common.swift
+//  XMGTV
+//
+//  Created by 蒋俊 on 17/3/30.
+//  Copyright © 2017年 coderwhy. All rights reserved.
+//
+
+import UIKit
+
+
+let kStatusBarH : CGFloat = 20
+let kNavigationBarH : CGFloat = 44
+let kTabbarH : CGFloat = 44
+
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
